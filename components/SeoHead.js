ef79ed16a60caeb32801be80665515cf397e98c6
ekaone <ekaone3033@gmail.com>
2022-12-16 13:46:24 +0700

    update seo

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -6,7 +6,7 @@ const defaultMeta = {
   title: "LaslesVPN",
   siteName: "LaslesVPN",
   description:
-    "Landing page VPN LaslesVPN Best VPN For Privacy, Country and Cheapest",
+    "PT GAP as Partner Design and Interior solution for your business",
   // change base url of your web (without '/' at the end)
   url: "https://next-landing-vpn.vercel.app",
   type: "website",
@@ -14,7 +14,7 @@ const defaultMeta = {
   // change with url of your image (recommended dimension = 1.91:1)
   // used in twitter, facebook, etc. card when link copied in tweet/status
   image: "https://pt-gap-com.vercel.app/assets/card-image.png",
-  author: "Lorem Ipsum",
+  author: "Eka",
 };
 
 /**
